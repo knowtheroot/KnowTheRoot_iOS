4cc003ed00ed6db965a4ddcd969000637225ac0d
陈家黎 <chenjiali01@souche.com>
2019-04-12 11:17:39 +0800

    update project

---
+++ Project/NotificationExtensionTest/MyserviceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  MyserviceExtension
+//
+//  Created by 陈家黎 on 2019/4/12.
+//  Copyright © 2019 Cooper. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
---
+++ Project/NotificationExtensionTest/MyserviceExtension/NotificationService.m
@@ -0,0 +1,39 @@
+//
+//  NotificationService.m
+//  MyserviceExtension
+//
+//  Created by 陈家黎 on 2019/4/12.
+//  Copyright © 2019 Cooper. All rights reserved.
+//
+
+#import "NotificationService.h"
+
+@interface NotificationService ()
+
+@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
+@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+
+@end
+
+@implementation NotificationService
+
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+    self.contentHandler = contentHandler;
+    self.bestAttemptContent = [request.content mutableCopy];
+    
+    // Modify the notification content here...
+    self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ [modified]", self.bestAttemptContent.title];
+    self.bestAttemptContent.body = @"我是新修改的body";
+    self.bestAttemptContent.title = @"我是新修改的title";
+    self.bestAttemptContent.subtitle = @"我是新修改的subtitle";
+    
+    self.contentHandler(self.bestAttemptContent);
+}
+
+- (void)serviceExtensionTimeWillExpire {
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    self.contentHandler(self.bestAttemptContent);
+}
+
+@end
