599a1477efabfa080b1a0185f50f6f96039c0709
陈家黎 <chenjiali01@souche.com>
2019-04-14 15:24:58 +0800

    add xmind

--- Project/NSStringAndNSMutableString/NSStringAndNSMutableString/ViewController.m
+++ Project/NSStringAndNSMutableString/NSStringAndNSMutableString/ViewController.m
@@ -18,10 +18,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    NSString *str1 = @"123";
-    NSLog(@"address = %p",str1);
-    NSString *str2 = [NSString stringWithFormat:@"123"];
-    NSLog(@"address = %p",str2);
+//    NSString *str1 = @"test";
+//    NSString *str2 = [str1 copy];
+//    NSString *str3 = [str1 mutableCopy];
+//    NSLog(@"str1 address = %p", str1);
+//    NSLog(@"str2 address = %p", str2);
+//    NSLog(@"str3 address = %p", str3);
+    
+//    NSString *str1 = @"123";
+//    NSString *str2 = @"123";
+//    NSLog(@"str1 address = %p", str1);
+//    NSLog(@"str2 address = %p", str2);
+    
+    NSMutableString *mutableStr1 = [[NSMutableString alloc] initWithString:@"123"];
+    NSMutableString *mutableStr2 = [[NSMutableString alloc] initWithString:@"123"];
+    NSLog(@"mutableStr1 address = %p", mutableStr1);
+    NSLog(@"mutableStr2 address = %p", mutableStr2);
+    
 }
 
 
